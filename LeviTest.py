3737b58a8f58baaf2e5510f0c3d0efc8bbb8f2b6
Gerardo Castillo <32140109+altersis@users.noreply.github.com>
2021-03-15 18:25:34 -0400

    first test

---
+++ LeviTest.py
@@ -0,0 +1,59 @@
+import asyncio
+import time
+
+import aiohttp
+# from aioleviosa import (
+#     LeviosaShadeGroup as tGroup,
+#     LeviosaZoneHub as tHub,
+#     discover_leviosa_zones as discover_leviosa_zones,
+# )
+import aioleviosa as lev
+import async_timeout
+
+
+async def Test(loop):
+    session = aiohttp.ClientSession()
+    print("Looking for Leviosa Zones (20 seconds)")
+    zones_found = await lev.discover_leviosa_zones()
+    print("Leviosa Zones found: ", zones_found)
+    addresses = list(zones_found.values())
+    if len(addresses) > 0:
+        print("Testing with: ", addresses[0])
+        hub = lev.LeviosaZoneHub(addresses[0], "MyTestHub", loop, session)
+        print("Hub created")
+        await hub.getHubInfo()
+        print("GetInfo should be done now")
+        print("version:", hub.fwVer)
+        hub.AddGroup("Group ONE")
+        hub.AddGroup("Group TWO")
+        hub.AddGroup("Group THREE")
+        hub.AddGroup("Group FOUR")
+        hub.AddGroup("Group FIVE")
+        hub.AddGroup("Group SIX")
+        BG_num = len(hub.groups)
+        print("Number of Blind Groups: ", BG_num)
+        for BlindGroup in hub.groups:
+            print(BlindGroup.name)
+            print("Opening")
+            await BlindGroup.open()
+            time.sleep(2)
+            print("Stopping")
+            await BlindGroup.stop()
+            time.sleep(2)
+            print("Closing")
+            await BlindGroup.close()
+            print("Next up position")
+            await BlindGroup.up()
+            time.sleep(2)
+            print("Next down position")
+            await BlindGroup.down()
+            time.sleep(5)
+            print("\n")
+    else:
+        print("No Zones found, nothing to test")
+    print("End of run")
+    await session.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(Test(loop))
---
+++ aioleviosa.py
@@ -0,0 +1,248 @@
+"""Class containing the async http methods to connect to a Leviosa Shades Zone Hub"""
+
+import asyncio
+from ipaddress import IPv4Address
+import logging
+from typing import Any, Mapping, Optional, Sequence, Tuple, Union
+
+import aiohttp
+import async_timeout
+from async_upnp_client.advertisement import UpnpAdvertisementListener
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class LvsaApiError(Exception):
+    """General Api error. Means we have a problem communication with
+    the Leviosa hub."""
+
+    pass
+
+
+class LvsaApiResponseStatusError(LvsaApiError):
+    """Wrong http response error."""
+
+
+class LvsaApiConnectionError(LvsaApiError):
+    """Problem connecting to Leviosa hub."""
+
+
+async def check_response(response, valid_response_codes):
+    """Check the response for correctness."""
+    if response.status == 204:
+        return True
+    if response.status in valid_response_codes:
+        _js = await response.json()
+        return _js
+    else:
+        raise LvsaApiResponseStatusError(response.status)
+
+
+async def discover_leviosa_zones() -> dict:
+    """Listen for advertisements."""
+    source_ip = "0.0.0.0"
+    _LOGGER.debug("setting up discovery for Leviosa Zone @%s", source_ip)
+    source_ip = IPv4Address(source_ip)
+    ZonesFound = {}
+
+    async def on_notify(data: Mapping[str, Any]) -> None:
+        if (
+            data.get("USN", "notfound").find("urn:leviosa:device:wiShadeController:1")
+            > 0
+        ) and (data.get("_udn", "notfound") not in ZonesFound.keys()):
+            ip = data.get("_address", "NOADDR")
+            if ip == "NOADDR":
+                ip = data.get("_host", "0.0.0.0:1900") + ":"
+            ZonesFound[data["_udn"]] = ip[: ip.find(":")]
+            _LOGGER.debug(
+                "Found a Leviosa Zone %s @%s", data["_udn"], ZonesFound[data["_udn"]]
+            )
+
+    listener = UpnpAdvertisementListener(
+        on_alive=on_notify,
+        source_ip=source_ip,
+    )
+    _LOGGER.debug("starting listener for Leviosa motor shades")
+    await listener.async_start()
+    try:
+        _LOGGER.debug("Letting Zones dicovery run for 20 secs")
+        await asyncio.sleep(20)
+    except:
+        _LOGGER.debug("exception: ")
+        raise
+    finally:
+        await listener.async_stop()
+        _LOGGER.debug("stopped discovery for Leviosa motor shades")
+    return ZonesFound
+
+    # self._devices = await find_leviosa_zones()
+
+
+class LeviosaZoneHub:
+    """Represents and manages the interaction with a Leviosa Zone Hub"""
+
+    def __init__(self, hub_ip, hub_name, loop=None, websession=None, timeout=15):
+        self.hub_ip = hub_ip
+        self.hub_name = hub_name
+        self.hub_fw_v = "0.0.0"
+        self._timeout = timeout
+        self.groups = []
+        self.AddGroup("All groups")
+        if loop:
+            self.loop = loop
+        else:
+            self.loop = asyncio.get_event_loop()
+        if websession:
+            self.websession = websession
+        else:
+            self.websession = aiohttp.ClientSession(loop=self.loop)
+
+    @property
+    def fwVer(self):
+        return self.hub_fw_v
+
+    @property
+    def name(self):
+        return self.hub_name
+
+    @property
+    def BlindGroups(self):
+        return self.groups
+
+    async def post(self, url_frag: str):
+        """
+        POST a request to the Leviosa Zone HUB
+
+        :param url_frag: string with the command fragment
+        :return: nothing (at this moment all POSTed reqs expect nothin')
+        """
+        response = None
+        try:
+            url = "http://" + self.hub_ip + url_frag
+            with async_timeout.timeout(self._timeout, loop=self.loop):
+                _LOGGER.debug("url: %s", url)
+                response = await self.websession.post(url)
+                _LOGGER.debug("return code is: %d", response.status)
+        except (asyncio.TimeoutError, aiohttp.ClientError) as error:
+            _LOGGER.error("Failed to communicate with Leviosa hub: %s", error)
+            raise LvsaApiConnectionError
+        finally:
+            if response is not None:
+                await response.release()
+
+    async def get(self, url_frag: str) -> dict:
+        """
+        Get a resource.
+
+        :param url_frag: string with the request fragment
+        :return: a dictionary with the HUB info
+        """
+        response = None
+        url = "http://" + self.hub_ip + "/" + url_frag
+        try:
+            _LOGGER.debug("Sending GET request to: %s" % url)
+            with async_timeout.timeout(self._timeout, loop=self.loop):
+                response = await self.websession.get(url)
+            if response.status == 200:
+                data = await response.json(content_type=None)
+                return data
+            else:
+                raise LvsaApiResponseStatusError(response.status)
+        except (asyncio.TimeoutError, aiohttp.ClientError) as error:
+            _LOGGER.error("Failed to communicate with Leviosa hub: %s", error)
+            raise LvsaApiConnectionError
+        finally:
+            if response is not None:
+                await response.release()
+
+    async def getHubInfo(self):
+        _LOGGER.debug("Getting HUB info from: %s", self.hub_ip)
+        hub_resp = await self.get("")  # Query the root doc of the Hub to get Hub info
+        if hub_resp:
+            self.hub_fw_v = hub_resp["firmware"]
+        else:
+            self.hub_fw_v = "invalid"
+
+    def AddGroup(self, GroupName):
+        newGroup = LeviosaShadeGroup(len(self.groups), GroupName, self)
+        _LOGGER.debug(
+            "Added new Group %s(%d) to Leviosa hub: %s",
+            GroupName,
+            len(self.groups),
+            self.hub_name,
+        )
+        self.groups.append(newGroup)
+        # print(len(self.groups) )
+        return newGroup
+
+
+class LeviosaShadeGroup:
+    can_move = True
+    can_tilt = False
+
+    def __init__(self, number, name, Hub: LeviosaZoneHub):
+        self.Hub = Hub
+        self.Number = number
+        self.Name = name
+        self.Position = 0
+
+    @property
+    def name(self):
+        return self.Name
+
+    @property
+    def number(self):
+        return self.Number
+
+    @property
+    def position(self):
+        return self.Position
+
+    async def move(self, target_position):
+        if target_position == 0:
+            command = "close"
+            self.Position = 0
+        else:
+            command = "open"
+            self.Position = 100
+        command = "/command/" + command + "/" + str(self.Number)
+        _LOGGER.debug(
+            "Setting cover.%s to position: %d",
+            self.Name,
+            self.Position,
+        )
+        result = await self.Hub.post(command)
+        return result
+
+    async def move_next_pos(self, direction):
+        if direction:
+            command = "down"
+        else:
+            command = "up"
+        self.Position = 50
+        _LOGGER.debug(
+            "Moving cover.%s to next %s poisition",
+            self.Name,
+            self.Position,
+        )
+        command = "/command/" + command + "/" + str(self.Number)
+        result = await self.Hub.post(command)
+        return result
+
+    async def close(self):
+        return await self.move(0)
+
+    async def down(self):
+        return await self.move_next_pos(True)
+
+    async def open(self):
+        return await self.move(100)
+
+    async def up(self):
+        return await self.move_next_pos(False)
+
+    async def stop(self):
+        """Stop the shade."""
+        command = "/command/stop/" + str(self.Number)
+        _LOGGER.debug("stopping cover.%s", self.Name)
+        return await self.Hub.post(command)
