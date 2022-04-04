6d09ce3f42decd9012246bad75afafd0fdb31fb0
Gerardo Castillo <32140109+altersis@users.noreply.github.com>
2022-04-03 22:24:23 -0400

    Made compatible with async_upnp_client 0.27.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'aioleviosa',         # This package talks to a Leviosa Zone hub
   packages = ['aioleviosa'],   
-  version = '0.3.4',           # Removed the need for 'ipaddress'
+  version = '0.3.5',           # Use source param in SsdpAdvertisementListener()
   license='APACHE 2.0',        # https://help.github.com/articles/licensing-a-repository
   description = 'AsyncIO compatible library to talk to a Leviosa Motor Shades Zone',   
   author = 'Gerardo Castillo',
